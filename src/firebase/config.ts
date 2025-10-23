979b6cc54e8d886329002a177c39a6ceeccd3a49
lahmalajanjiomar-sketch <lahmalajanjiomar@gmail.com>
2025-10-23 21:51:17 +0300

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  "apiKey": "AIzaSyCV3T_piyG-a11Lp5gS5Zz-a70sx2yTrp8",
-  "authDomain": "khsayem-tekstil.firebaseapp.com",
-  "projectId": "khsayem-tekstil",
-  "storageBucket": "khsayem-tekstil.appspot.com",
-  "messagingSenderId": "390185499982",
-  "appId": "1:390185499982:web:65433a5953a748a0a473e3"
-};
